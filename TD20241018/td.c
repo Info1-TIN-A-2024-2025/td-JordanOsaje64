309e8b85c606433be48a6b3f22f7afdb74ed4bc9
JordanOsaje64 <jordan.osaje@heig-vd.ch>
2024-10-18 12:44:11 +0000

    maj

--- TD20241018/td.c
+++ TD20241018/td.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h> // Pour atoi
 int main(int argc, const char *argv[])
 {
 	//display the value of argc
@@ -18,10 +18,38 @@ int main(int argc, const char *argv[])
 	{
 		printf("Erreur, mauvais nombre d'arguments\n");
 		return 1;
-	}*/
+	}
 	printf("argv[0] = %s\n", argv[0]);
 	printf("argv[1] = %s\n", argv[1]);
 	printf("argv[2] = %s\n", argv[2]);
+	
+	int i =0;
+	double x = 0;
+
+	i = atoi(argv[1]);
+	x = atof(argv[2]);
+	printf("I = %d, x = %f\n",i,x);
+
+	int i = 42;
+	printf("i = %d\n",i);
+	i++;	//equivalent de i = i +1
+	printf("i = %d\n",i);*/
+
+	int i = 57;
+	printf("i = %d\n",i);	//57
+	int j = 0;
+	j=i++;	//post incrémentation
+	printf("j = %d\n",j);	//58
+	printf("i = %d\n",i);	//58
+
+	puts("______________________");
+
+	i = 57;
+	printf("i = %d\n",i);	//57
+	j = 0;
+	j=++i;	//pre incrémentation
+	printf("j = %d\n",j);	//58
+	printf("i = %d\n",i);	//58
 
 	return 0;
 }
